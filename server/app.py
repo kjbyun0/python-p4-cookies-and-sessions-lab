9308e0a8088f25f25314c8d3ea0798cc91a43baa
KI JONG BYUN <kjbyun0@gmail.com>
2024-03-11 13:04:42 -0500

    Assignment Done

--- server/app.py
+++ server/app.py
@@ -22,13 +22,22 @@ def clear_session():
 
 @app.route('/articles')
 def index_articles():
-
-    pass
+    records_dict = [record.to_dict() for record in Article.query.all()]
+    return make_response(records_dict, 200)
 
 @app.route('/articles/<int:id>')
 def show_article(id):
+    session['page_views'] = session.get('page_views') or 0
+    session['page_views'] += 1
 
-    pass
+    if session['page_views'] <= 3:
+        record = Article.query.filter_by(id=id).first()
+        return make_response(record.to_dict(), 200,)
+    
+    return make_response(
+        {'message': 'Maximum pageview limit reached'},
+        401,
+    )
 
 if __name__ == '__main__':
     app.run(port=5555)
